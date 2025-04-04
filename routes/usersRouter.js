3ee55f0faaf935369e9edf3ae869c72354bd4be6
Devanshu Singh <dev2003.rajput@gmail.com>
2025-04-04 19:05:52 +0530

    Done some jwt, bcrypt and try catch algorithm in /register route

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,8 +1,38 @@
 const express = require('express')
 const router = express.Router()
+const bcrypt = require('bcrypt')
+const jwt = require("jsonwebtoken")
 
-router.get("/", (req, res)=>{
+router.get("/", (req, res) => {
     res.send("hey it's working")
+})
+
+router.post("/register", (req, res) => {
+    try {
+        let { email, password, fullname } = req.body;
+
+        bcrypt.genSalt(10, (err, salt) => {
+            bcrypt.hash(password, salt, async (err, hash) => {
+                if (err) return res.send(err.message)
+                else {
+                    let user = await userModel.create({
+                        email,
+                        password: hash,
+                        fullname,
+                    })
+
+                    let token = jwt.sign({email, id: user._id}, "hehehehe");
+                    res.cookie("token", token)
+                    res.send("User created successfully")
+                }
+            })
+        })
+
+
+    } catch (err) {
+        res.send(err.message);
+
+    }
 })
 
 module.exports = router;
