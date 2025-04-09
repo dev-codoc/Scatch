d422691c4490c61118cd2394005d6dff2b5861d2
Devanshu Singh <dev2003.rajput@gmail.com>
2025-04-10 00:24:28 +0530

    Done the Express session secret , flash concept introduced bcrypt for the login and register , also tested via POSTMAN ## Done some error handling in the index.ejs file

--- app.js
+++ app.js
@@ -3,16 +3,29 @@ const app = express()
 const cookieParser = require('cookie-parser')
 const path = require('path')
 const db = require('./config/mongoose-connection')
+const expressSession = require('express-session')
+const flash = require('connect-flash')
+
+require("dotenv").config();
+
+// Routes
 const ownersRouter = require("./routes/ownersRouter")
 const usersRouter = require("./routes/usersRouter")
 const productsRouter = require("./routes/productsRouter")
 const indexRouter = require("./routes/index")
 
-require("dotenv").config();
 
 app.use(express.json())
 app.use(express.urlencoded({extended : true}))
 app.use(cookieParser())
+app.use(
+    expressSession({
+        secret: process.env.EXPRESS_SESSION_SECRET || "secret",
+        resave: false,
+        saveUninitialized: false,
+    })
+)
+app.use(flash())
 app.use(express.static(path.join(__dirname, "public")))
 app.set("view engine", "ejs")
 
