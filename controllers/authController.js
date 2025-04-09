16b542e0ec67cf9d50c0d29b6af23d6ba67e54a0
Devanshu Singh <dev2003.rajput@gmail.com>
2025-04-09 23:44:12 +0530

    Done the Express session secret , flash concept introduced bcrypt for the login and register , also tested via POSTMAN ## Done some error handling in the index.ejs file

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,9 +3,13 @@ const bcrypt = require('bcrypt')
 const jwt = require("jsonwebtoken")
 const generateToken = require("../utils/generateToken")
 
-module.exports.registerUser = (req, res) => {
+module.exports.registerUser = async (req, res) => {
     try {
         let { email, password, fullname } = req.body;
+
+
+        let user = await userModel.findOne({ email:email })
+        if (user) return res.status(400).send("User already exists")
 
         bcrypt.genSalt(10, (err, salt) => {
             bcrypt.hash(password, salt, async (err, hash) => {
@@ -28,5 +32,28 @@ module.exports.registerUser = (req, res) => {
     } catch (err) {
         res.send(err.message);
 
+    }
+}
+
+module.exports.loginUser = async (req, res) => {
+
+    try{
+        let {email, password} = req.body;
+
+        let user = await userModel.findOne({email: email})
+        if(!user) return res.status(401).send("1Invalid email or password")
+
+        bcrypt.compare(password, user.password, (err, result) => {
+            if(result) {
+                let token = generateToken(user);
+                res.cookie("token", token)
+                res.send("Logged in successfully")
+            }
+            else {
+                return res.status(401).send("2Invalid email or password")
+            }
+        })
+    } catch(err) {
+        res.send(err.message);
     }
 }
